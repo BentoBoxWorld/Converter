4002c78471cddb1307cf0b7e63f1925f4364c3c5
tastybento <tastybento@wasteofplastic.com>
2019-05-02 07:34:35 -0700

    Basic converting working.

--- src/main/java/world/bentobox/a2b/A2B.java
+++ src/main/java/world/bentobox/a2b/A2B.java
@@ -3,52 +3,38 @@ package world.bentobox.a2b;
 import java.io.File;
 import java.io.IOException;
 
-import org.bukkit.World;
-import org.bukkit.WorldCreator;
-import org.bukkit.WorldType;
-import org.bukkit.World.Environment;
-import org.bukkit.generator.ChunkGenerator;
-import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.jdt.annotation.Nullable;
-
 import com.google.common.io.Files;
 
-import world.bentobox.a2b.commands.AdminCommand;
-import world.bentobox.a2b.world.ChunkGeneratorWorld;
-import world.bentobox.bentobox.api.addons.GameModeAddon;
-import world.bentobox.bentobox.api.configuration.Config;
-import world.bentobox.bentobox.api.configuration.WorldSettings;
+import world.bentobox.a2b.commands.AdminConvertCommand;
+import world.bentobox.bentobox.api.addons.Addon;
+import world.bentobox.bskyblock.BSkyBlock;
 
 /**
  * Convert ASkyBlock to BSkyBlock
  * @author tastybento
  */
-public class A2B extends GameModeAddon {
-
-    private static final String NETHER = "_nether";
-    private static final String THE_END = "_the_end";
-
-    // Settings
-    private ChunkGeneratorWorld chunkGenerator;
-    private @Nullable Settings settings;
-    private World islandWorld2;
-    private Object netherWorld2;
-    private Object endWorld2;
-
+public class A2B extends Addon {
 
     @Override
     public void onLoad() {
-        // Save the default config from config-converted.yml
-        settings = new Settings();
-        this.saveWorldSettings();
-        // Settings
+        // Import settings
         importASkyBlockconfig();
     }
 
     @Override
     public void onEnable(){
-        // Register commands
-        adminCommand = new AdminCommand(this);
+        if(getAddonByName("BSkyBlock").map(BSkyBlock.class::cast).map(gm -> {
+            // Register commands
+            gm.getAdminCommand().ifPresent(adminCommand ->  {
+                new AdminConvertCommand(adminCommand, gm);
+            });
+            return false;
+        }).orElse(true)) {
+            logError("a2b requires BSkyBlock addon to be present! Disabling...");
+            this.setState(State.DISABLED);
+            return;
+        }
+
     }
 
     private void importASkyBlockconfig() {
@@ -80,104 +66,6 @@ public class A2B extends GameModeAddon {
 
     @Override
     public void onReload() {
-    }
-
-    @Override
-    public void createWorlds() {
-        // ASkyBlock
-        String worldName = getConfig().getString("general.worldName", "ASkyBlock");
-        boolean createNether = getConfig().getBoolean("general.createnether");
-        boolean newNether = getConfig().getBoolean("general.newnether");
-        // Open up the world
-        ChunkGeneratorWorld chunkGenerator = new ChunkGeneratorWorld();
-        islandWorld = WorldCreator.name(worldName).type(WorldType.FLAT).environment(World.Environment.NORMAL).generator(chunkGenerator).createWorld();
-
-        // Get nether
-        if (createNether && newNether) {
-            netherWorld = WorldCreator.name(worldName + NETHER).type(WorldType.FLAT).environment(World.Environment.NORMAL).generator(chunkGenerator).createWorld();
-        }
-
-        // BSkyBlock
-        String worldName2 = settings.getWorldName();
-        if (getServer().getWorld(worldName2) == null) {
-            log("Creating BSkyBlock world ...");
-        }
-        chunkGenerator = settings.isUseOwnGenerator() ? null : new ChunkGeneratorWorld();
-        // Create the world if it does not exist
-        islandWorld2 = getWorld(worldName, World.Environment.NORMAL, chunkGenerator);
-
-        // Make the nether if it does not exist
-        if (settings.isNetherGenerate()) {
-            if (getServer().getWorld(worldName + NETHER) == null) {
-                log("Creating BSkyBlock's Nether...");
-            }
-            netherWorld2 = settings.isNetherIslands() ? getWorld(worldName, World.Environment.NETHER, chunkGenerator) : getWorld(worldName, World.Environment.NETHER, null);
-        }
-        // Make the end if it does not exist
-        if (settings.isEndGenerate()) {
-            if (getServer().getWorld(worldName + THE_END) == null) {
-                log("Creating BSkyBlock's End World...");
-            }
-            endWorld2 = settings.isEndIslands() ? getWorld(worldName, World.Environment.THE_END, chunkGenerator) : getWorld(worldName, World.Environment.THE_END, null);
-        }
-    }
-
-    /**
-     * Gets a world or generates a new world if it does not exist
-     * @param worldName - the overworld name
-     * @param env - the environment
-     * @param chunkGenerator2 - the chunk generator. If <tt>null</tt> then the generator will not be specified
-     * @return world loaded or generated
-     */
-    private World getWorld(String worldName, Environment env, ChunkGeneratorWorld chunkGenerator2) {
-        // Set world name
-        worldName = env.equals(World.Environment.NETHER) ? worldName + NETHER : worldName;
-        worldName = env.equals(World.Environment.THE_END) ? worldName + THE_END : worldName;
-        WorldCreator wc = WorldCreator.name(worldName).type(WorldType.FLAT).environment(env);
-        return settings.isUseOwnGenerator() ? wc.createWorld() : wc.generator(chunkGenerator2).createWorld();
-    }
-
-    @Override
-    public WorldSettings getWorldSettings() {
-        return settings;
-    }
-
-    @Override
-    public @NonNull ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
-        return chunkGenerator;
-    }
-
-    @Override
-    public void saveWorldSettings() {
-        if (settings != null) {
-            new Config<>(this, Settings.class).saveConfigObject(settings);
-        }
-
-    }
-
-    public Settings getSettings() {
-        return settings;
-    }
-
-    /**
-     * @return the islandWorld2
-     */
-    public World getIslandWorld2() {
-        return islandWorld2;
-    }
-
-    /**
-     * @return the netherWorld2
-     */
-    public Object getNetherWorld2() {
-        return netherWorld2;
-    }
-
-    /**
-     * @return the endWorld2
-     */
-    public Object getEndWorld2() {
-        return endWorld2;
     }
 
 }
--- src/main/java/world/bentobox/a2b/commands/AdminConvertCommand.java
+++ src/main/java/world/bentobox/a2b/commands/AdminConvertCommand.java
@@ -8,13 +8,15 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.bukkit.block.Biome;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.EntityType;
 
-import world.bentobox.a2b.A2B;
-import world.bentobox.a2b.Settings;
 import world.bentobox.bentobox.api.commands.CompositeCommand;
 import world.bentobox.bentobox.api.user.User;
 import world.bentobox.bentobox.database.Database;
@@ -25,13 +27,15 @@ import world.bentobox.bentobox.lists.Flags;
 import world.bentobox.bentobox.managers.PlayersManager;
 import world.bentobox.bentobox.managers.RanksManager;
 import world.bentobox.bentobox.util.Util;
+import world.bentobox.bskyblock.BSkyBlock;
+import world.bentobox.bskyblock.Settings;
 
 public class AdminConvertCommand extends CompositeCommand {
 
+    private final static List<String> ENTITIES = Arrays.asList(EntityType.values()).stream().map(EntityType::name).collect(Collectors.toList());
+
     private Database<Players> handler;
     private Database<Names> names;
-
-    private A2B gm;
 
     // Owner, island
     Map<UUID, Island> islands = new HashMap<>();
@@ -46,10 +50,11 @@ public class AdminConvertCommand extends CompositeCommand {
     private int islandStartZ;
     private int islandHeight;
     private int seaHeight;
+    private BSkyBlock gm;
 
-    public AdminConvertCommand(CompositeCommand parent) {
+    public AdminConvertCommand(CompositeCommand parent, BSkyBlock gm) {
         super(parent, "convert");
-        this.gm = (A2B)getAddon();
+        this.gm = gm;
     }
 
     @Override
@@ -64,11 +69,41 @@ public class AdminConvertCommand extends CompositeCommand {
         names = new Database<>(getPlugin(), Names.class);
     }
 
+    @Override
+    public boolean canExecute(User user, String label, List<String> args) {
+        if (!args.isEmpty()) {
+            // Show help
+            showHelp(this, user);
+            return false;
+        }
+        // Load ASkyBlock world
+        File ASBconfig = new File(getPlugin().getDataFolder(), "../ASkyBlock/config.yml");
+        if (!ASBconfig.exists()) {
+            user.sendRawMessage("Cannot find ASkyBlock config.yml file!");
+            return false;
+        }
+        config = new YamlConfiguration();
+        try {
+            config.load(ASBconfig);
+        } catch (IOException | InvalidConfigurationException e) {
+            user.sendRawMessage("Cannot load ASkyBlock config.yml file! " + e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
 
     @Override
     public boolean execute(User user, String label, List<String> args) {
         user.sendRawMessage("Getting the world settings");
-        getConfigs();
+        try {
+            getConfigs(user);
+        } catch (Exception e) {
+            getAddon().logError("Config conversion error: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
         user.sendRawMessage("Converting players");
         try {
             convertplayers(user);
@@ -86,22 +121,45 @@ public class AdminConvertCommand extends CompositeCommand {
         return object;
     }
 
-    private void getConfigs() {
+    private void getConfigs(User user) throws InvalidConfigurationException {
+        String worldName;
         // Get config items for world
-        try {
-            islandDistance = nullCheck(config.getInt("island.distance"));
-            islandProtectionRange = nullCheck(config.getInt("island.protectionRange"));
-            islandXOffset = nullCheck(config.getInt("island.xoffset"));
-            islandZOffset = nullCheck(config.getInt("island.zoffset"));
-            islandStartX = nullCheck(config.getInt("island.startx"));
-            islandStartZ = nullCheck(config.getInt("island.startz"));
-            islandHeight = nullCheck(config.getInt("island.islandlevel")) - 5;
-            seaHeight = nullCheck(config.getInt("island.sealevel"));
-        } catch (Exception e) {
-            getAddon().logError(e.getMessage());
-            return;
-        }
+        //try {
+        // World settings
+        worldName = config.getString("general.worldName");
+        // Island settings
+        islandDistance = nullCheck(config.getInt("island.distance"));
+        islandProtectionRange = nullCheck(config.getInt("island.protectionRange"));
+        islandXOffset = nullCheck(config.getInt("island.xoffset"));
+        islandZOffset = nullCheck(config.getInt("island.zoffset"));
+        islandStartX = nullCheck(config.getInt("island.startx"));
+        islandStartZ = nullCheck(config.getInt("island.startz"));
+        islandHeight = nullCheck(config.getInt("island.islandlevel")) - 5;
+        seaHeight = nullCheck(config.getInt("island.sealevel"));
+        //} catch (Exception e) {
+        //    getAddon().logError(e.getMessage());
+        //    return;
+        //}
+
+        // World or general settings
         Settings s = gm.getSettings();
+        s.setWorldName(worldName);
+        s.setMaxIslands(config.getInt("general.maxIslands"));
+        s.setNetherGenerate(config.getBoolean("general.createnether", true));
+        s.setNetherIslands(config.getBoolean("general.newnether", true));
+        s.setNetherTrees(config.getBoolean("general.nethertrees", true));
+        s.setNetherRoof(config.getBoolean("general.netherroof", true));
+        s.setNetherSpawnRadius(config.getInt("general.netherspawnradius", 25));
+        s.setVisitorBannedCommands(config.getStringList("general.visitorbannedcommands"));
+        s.setOnJoinResetInventory(config.getBoolean("general.resetinventory"));
+        s.setKickedKeepInventory(config.getBoolean("general.kickedkeepinv", true));
+        s.setRemoveMobsWhitelist(cleanEntities(config.getStringList("general.mobwhitelist")));
+        s.setLeaversLoseReset(config.getBoolean("general.leaversloseresets"));
+        s.setOnJoinResetEnderChest(config.getBoolean("general.resetenderchest"));
+        s.setMaxHomes(config.getInt("general.maxhomes",1));
+        s.setDefaultBiome(cleanBiome(config.getString("biomesettings.defaultbiome", "PLAINS")));
+
+        // Island settings
         s.setIslandXOffset(islandXOffset);
         s.setIslandZOffset(islandZOffset);
         s.setIslandDistance(islandDistance / 2);
@@ -110,8 +168,17 @@ public class AdminConvertCommand extends CompositeCommand {
         s.setIslandStartZ(islandStartZ);
         s.setSeaHeight(seaHeight != 0 ? seaHeight - 1 : 0);
         s.setIslandHeight(islandHeight);
+        user.sendRawMessage("Saving BSkyBlock config");
         gm.saveWorldSettings();
 
+    }
+
+    private Biome cleanBiome(String string) {
+        return Arrays.asList(Biome.values()).stream().filter(b -> b.name().equals(string.toUpperCase())).findFirst().orElse(Biome.PLAINS);
+    }
+
+    private Set<EntityType> cleanEntities(List<String> stringList) {
+        return stringList.stream().filter(ENTITIES::contains).map(EntityType::valueOf).collect(Collectors.toSet());
     }
 
     private void convertplayers(User user) throws InvalidConfigurationException, FileNotFoundException, IOException {
